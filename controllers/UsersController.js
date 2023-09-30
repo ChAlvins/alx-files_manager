9254a03fe67c159958ca4cac53b9c4248b1755f6
vpnchengo <charo.alvin@gmail.com>
2023-09-30 05:24:51 -0700

    added new enpoint retriving user based on token

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,5 +1,7 @@
 import sha1 from 'sha1';
+import { ObjectID } from 'mongodb';
 import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 class UsersController {
   static async postNew(req, res) {
@@ -36,6 +38,26 @@ class UsersController {
     };
 
     return res.status(201).json(newUser);
+  }
+
+  static async getMe(request, response) {
+    const token = request.header('X-Token');
+    const key = `auth_${token}`;
+    const userId = await redisClient.get(key);
+    if (userId) {
+      const users = dbClient.db.collection('users');
+      const idObject = new ObjectID(userId);
+      users.findOne({ _id: idObject }, (err, user) => {
+        if (user) {
+          response.status(200).json({ id: userId, email: user.email });
+        } else {
+          response.status(401).json({ error: 'Unauthorized' });
+        }
+      });
+    } else {
+      console.log('unreachable');
+      response.status(401).json({ error: 'Unauthorized' });
+    }
   }
 }
 
